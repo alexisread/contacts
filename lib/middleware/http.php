90337a4aaf2e36376b55ac8ed0d34991bd76a9ed
alexisread <alexis.read@gmail.com>
2014-05-08 17:21:11 +0100

    Missed a ref

--- lib/middleware/http.php
+++ lib/middleware/http.php
@@ -25,7 +25,7 @@
 namespace OCA\Contacts\Middleware;
 
 use OCA\Contacts\Controller\Controller,
-	OCA\Contacts\JSONResponse,
+	OCA\Contacts\Service\JSONResponse,
 	OCP\AppFramework\Middleware,
 	OCP\AppFramework\Http\Response,
 	OCP\AppFramework\Http as HttpStatus;
